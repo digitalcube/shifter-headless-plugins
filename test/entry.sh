82f983990c8ae5392fc3db67375b2de2996b3a84
sawanoboly <sawanoboriyu@higanworks.com>
2020-08-18 15:53:56 +0900

    ref: Automated repository tests

--- test/entry.sh
+++ test/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# exec bash -c "sleep 3600"
+find plugins/ -type d -name vendor -prune -o -type f -name "*.php" -print0 | xargs -n 1 -0 php -l
 
-./vendor/bin/phpcs --config-set installed_paths "$(pwd)/vendor/wp-coding-standards/wpcs"
-./vendor/bin/phpcs -p -s -v --standard=WordPress /srv/plugins
+# ./vendor/bin/phpcs --config-set installed_paths "$(pwd)/vendor/wp-coding-standards/wpcs"
+# ./vendor/bin/phpcs -p -s -v --standard=WordPress /srv/plugins
