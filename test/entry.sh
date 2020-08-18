20bc6a913cdc97783d2d89f9a46c9af44e30798d
sawanoboly <sawanoboriyu@higanworks.com>
2020-08-18 16:43:26 +0900

    add phpcs

--- test/entry.sh
+++ test/entry.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+env
+if [ "$REPORT" == "1" ]; then
+  ./vendor/bin/phpcs --config-set installed_paths "$(pwd)/vendor/wp-coding-standards/wpcs"
+  # ./vendor/bin/phpcs -snp --no-colors --report=source --report-diff=/srv/results/report-diff.log --report-full=/srv/results/report-full.log --standard=./phpcs.xml
+  ./vendor/bin/phpcs -snp --no-colors --report=source --report-summary=/srv/results/report-summary.log --report-full=/srv/results/report-full.log --standard=./phpcs.xml
+fi
 
 find plugins/ -type d -name vendor -prune -o -type f -name "*.php" -print0 | xargs -n 1 -0 php -l
 
-# ./vendor/bin/phpcs --config-set installed_paths "$(pwd)/vendor/wp-coding-standards/wpcs"
-# ./vendor/bin/phpcs -p -s -v --standard=WordPress /srv/plugins
